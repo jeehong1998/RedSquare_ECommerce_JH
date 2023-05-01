bae1f4929f9a79a1b0c8ed4f9c5c5eab01d85523
Jee Hong <jeehong980923@gmail.com>
2023-05-01 11:57:46 +0800

    - Develop a page that displays a list of products, including their name, price, image, category, etc. - One page should display at most 10 product only

--- src/ProductListing.jsx
+++ src/ProductListing.jsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
-import Axios from 'axios';
 
 export const ProductListing = (props) => 
 {
+    const [product, setProduct] = useState('');
+
+    const generate_products = async () => 
+    {
+        const response = await fetch('https://dummyjson.com/products');
+        const res = await response.json();
+
+        const products = res.products.slice(0, 10).map((element, index) => (
+            <div className="product" key = {index}>
+                <img src={element.images[0]} alt={element.title} />
+                <h2>{element.title}</h2>
+                <p className="category">{element.category}</p>
+                <p className="price">RM {element.price}</p>
+            </div>
+        ));
+
+        setProduct(products);
+    };
+
+    generate_products();
+
     return (
-        <h2>Product Listing</h2>
-    );
+        <div className="product-list">
+            <div className="header">
+                <h1>Product List</h1>
+                <button className="logout-button">Logout</button>
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name" />
+            </div>
+            <div className="products">
+                {product}
+            </div>
+        </div>
+    )
 }
